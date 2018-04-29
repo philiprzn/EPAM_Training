3a01fe223168eb267c3bd705136a124a6a29080c
Philip Maksiutov <philip_bmx11@yahoo.com>
2018-04-29 17:38:01 +0300

    popup

---
+++ library/js/main.js
@@ -0,0 +1,11 @@
+
+// ///////////  Popup \\\\\\\\\\\
+
+var popup = document.getElementById('popup')
+function appearance() {
+    popup.style.display="block";
+};
+function disappearance() {
+    popup.style.display="none";
+}
+
